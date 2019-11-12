ded3327e5d840ed7b1649d7160e7aecc6c0bdd6c
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-11-12 23:11:03 +0100

    methods are created and added in server.js

--- server.js
+++ server.js
@@ -6,11 +6,44 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
+app.get("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, mes: "Show all bootcamps" });
+});
+
+app.get("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, mes: `Show a bootcamp ${req.params.id}` });
+});
+
+app.post("/api/v1/bootcamps", (req, res) => {
+  res.status(200).json({ success: true, mes: "Create new bootcamp" });
+});
+
+app.put("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, mes: `Update bootcamp ${req.params.id}` });
+});
+
+app.delete("/api/v1/bootcamps/:id", (req, res) => {
+  res
+    .status(200)
+    .json({ success: true, mes: `Delete bootcamp ${req.params.id}` });
+});
+
+// app.get("/", (req, res) => {
+//   // res.send("<h1>Hello from Express</h1>");
+
+//   //By the method you can set the status and send data as a JSON
+//   res.status(200).json({ success: true, data: { id: 1 } });
+// });
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
   console.log(
-    `Server is runnin in ${process.env.NODE_ENV} mode on port: ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port: ${PORT}`
   )
 );
