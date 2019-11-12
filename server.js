39665fc7dc58f77c07395346401aec05b549ac45
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-11-12 23:31:43 +0100

    making routers

--- server.js
+++ server.js
@@ -1,43 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
 // Load evn vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.get("/api/v1/bootcamps", (req, res) => {
-  res.status(200).json({ success: true, mes: "Show all bootcamps" });
-});
-
-app.get("/api/v1/bootcamps/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, mes: `Show a bootcamp ${req.params.id}` });
-});
-
-app.post("/api/v1/bootcamps", (req, res) => {
-  res.status(200).json({ success: true, mes: "Create new bootcamp" });
-});
-
-app.put("/api/v1/bootcamps/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, mes: `Update bootcamp ${req.params.id}` });
-});
-
-app.delete("/api/v1/bootcamps/:id", (req, res) => {
-  res
-    .status(200)
-    .json({ success: true, mes: `Delete bootcamp ${req.params.id}` });
-});
-
-// app.get("/", (req, res) => {
-//   // res.send("<h1>Hello from Express</h1>");
-
-//   //By the method you can set the status and send data as a JSON
-//   res.status(200).json({ success: true, data: { id: 1 } });
-// });
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
