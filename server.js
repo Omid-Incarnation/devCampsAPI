3ccaf58b09624b28268e288496a54473942f91d1
omid-incarnation <mohammad.omidi1988@gmail.com>
2019-11-17 02:00:15 +0100

    colors add to clg

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
 const connectDB = require("./config/db");
 
 // Load evn vars
@@ -13,6 +14,8 @@ connectDB();
 const bootcamps = require("./routes/bootcamps");
 
 const app = express();
+
+app.use(express.json());
 
 // Dev Logging middleware
 if (process.env.NODE_ENV === "development") {
@@ -27,7 +30,8 @@ const PORT = process.env.PORT || 5000;
 const server = app.listen(
   PORT,
   console.log(
-    `Server is running in ${process.env.NODE_ENV} mode on port: ${PORT}`
+    `Server is running in ${process.env.NODE_ENV} mode on port: ${PORT}`.blue
+      .bold
   )
 );
 
